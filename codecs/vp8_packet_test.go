d35e43f3b8f2fc1db5c1595f6b15c2a6035556f0
Antoine Baché <antoine.bache@epitech.eu>
2019-02-26 17:02:52 -0800

    Removed testing library dependency
    - Removed assert dependency from common tests
    - Removed assert dependency from vp8 tests
    
    Relates #1

--- codecs/vp8_packet_test.go
+++ codecs/vp8_packet_test.go
@@ -5,11 +5,9 @@ import (
 	"testing"
 
 	"github.com/pions/rtp"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestVP8Packet_Unmarshal(t *testing.T) {
-	assert := assert.New(t)
 	pck := VP8Packet{}
 
 	errSmallerThanHeaderLen := fmt.Errorf("Payload is not large enough to container header")
@@ -19,76 +17,120 @@ func TestVP8Packet_Unmarshal(t *testing.T) {
 	raw, err := pck.Unmarshal(&rtp.Packet{
 		Payload: nil,
 	})
-	assert.Nil(raw, "Result should be nil in case of error")
-	assert.Equal(err, errSmallerThanHeaderLen, "Error shouldn't nil in case of error")
+	if raw != nil {
+		t.Fatal("Result should be nil in case of error")
+	}
+	if err == nil || err.Error() != errSmallerThanHeaderLen.Error() {
+		t.Fatal("Error should be:", errSmallerThanHeaderLen)
+	}
 
 	// Empty payload
 	raw, err = pck.Unmarshal(&rtp.Packet{
 		Payload: nil,
 	})
-	assert.Nil(raw, "Result should be nil in case of error")
-	assert.Equal(err, errSmallerThanHeaderLen, "Error shouldn't nil in case of error")
+	if raw != nil {
+		t.Fatal("Result should be nil in case of error")
+	}
+	if err == nil || err.Error() != errSmallerThanHeaderLen.Error() {
+		t.Fatal("Error should be:", errSmallerThanHeaderLen)
+	}
 
 	// Payload smaller than header size
 	raw, err = pck.Unmarshal(&rtp.Packet{
 		Payload: []byte{0x00, 0x11, 0x22},
 	})
-	assert.Nil(raw, "Result should be nil in case of error")
-	assert.Equal(err, errSmallerThanHeaderLen, "Error shouldn't nil in case of error")
+	if raw != nil {
+		t.Fatal("Result should be nil in case of error")
+	}
+	if err == nil || err.Error() != errSmallerThanHeaderLen.Error() {
+		t.Fatal("Error should be:", errSmallerThanHeaderLen)
+	}
 
 	// Normal payload
 	raw, err = pck.Unmarshal(&rtp.Packet{
 		Payload: []byte{0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x90},
 	})
-	assert.NotNil(raw, "Result shouldn't be nil in case of succeess")
-	assert.Nil(err, "Error should be nil in case of success")
+	if raw == nil {
+		t.Fatal("Result shouldn't be nil in case of success")
+	}
+	if err != nil {
+		t.Fatal("Error should be nil in case of success")
+	}
 
 	// Header size, only X
 	raw, err = pck.Unmarshal(&rtp.Packet{
 		Payload: []byte{0x80, 0x00, 0x00, 0x00},
 	})
-	assert.NotNil(raw, "Result shouldn't be nil in case of succeess")
-	assert.Nil(err, "Error should be nil in case of success")
+	if raw == nil {
+		t.Fatal("Result shouldn't be nil in case of success")
+	}
+	if err != nil {
+		t.Fatal("Error should be nil in case of success")
+	}
 
 	// Header size, X and I
 	raw, err = pck.Unmarshal(&rtp.Packet{
 		Payload: []byte{0x80, 0x80, 0x00, 0x00},
 	})
-	assert.NotNil(raw, "Result shouldn't be nil in case of succeess")
-	assert.Nil(err, "Error should be nil in case of success")
+	if raw == nil {
+		t.Fatal("Result shouldn't be nil in case of success")
+	}
+	if err != nil {
+		t.Fatal("Error should be nil in case of success")
+	}
 
 	// Header size, X and I, PID 16bits
 	raw, err = pck.Unmarshal(&rtp.Packet{
 		Payload: []byte{0x80, 0x80, 0x81, 0x00},
 	})
-	assert.Nil(raw, "Result should be nil in case of error")
-	assert.Equal(err, errPayloadTooSmall, "Error shouldn't nil in case of error")
+	if raw != nil {
+		t.Fatal("Result should be nil in case of error")
+	}
+	if err == nil || err.Error() != errPayloadTooSmall.Error() {
+		t.Fatal("Error should be:", errPayloadTooSmall)
+	}
 
 	// Header size, X and L
 	raw, err = pck.Unmarshal(&rtp.Packet{
 		Payload: []byte{0x80, 0x40, 0x00, 0x00},
 	})
-	assert.NotNil(raw, "Result shouldn't be nil in case of succeess")
-	assert.Nil(err, "Error should be nil in case of success")
+	if raw == nil {
+		t.Fatal("Result shouldn't be nil in case of success")
+	}
+	if err != nil {
+		t.Fatal("Error should be nil in case of success")
+	}
 
 	// Header size, X and T
 	raw, err = pck.Unmarshal(&rtp.Packet{
 		Payload: []byte{0x80, 0x20, 0x00, 0x00},
 	})
-	assert.NotNil(raw, "Result shouldn't be nil in case of succeess")
-	assert.Nil(err, "Error should be nil in case of success")
+	if raw == nil {
+		t.Fatal("Result shouldn't be nil in case of success")
+	}
+	if err != nil {
+		t.Fatal("Error should be nil in case of success")
+	}
 
 	// Header size, X and K
 	raw, err = pck.Unmarshal(&rtp.Packet{
 		Payload: []byte{0x80, 0x10, 0x00, 0x00},
 	})
-	assert.NotNil(raw, "Result shouldn't be nil in case of succeess")
-	assert.Nil(err, "Error should be nil in case of success")
+	if raw == nil {
+		t.Fatal("Result shouldn't be nil in case of success")
+	}
+	if err != nil {
+		t.Fatal("Error should be nil in case of success")
+	}
 
 	// Header size, all flags
 	raw, err = pck.Unmarshal(&rtp.Packet{
 		Payload: []byte{0xff, 0xff, 0x00, 0x00},
 	})
-	assert.Nil(raw, "Result should be nil in case of error")
-	assert.Equal(err, errPayloadTooSmall, "Error shouldn't nil in case of error")
+	if raw != nil {
+		t.Fatal("Result should be nil in case of error")
+	}
+	if err == nil || err.Error() != errPayloadTooSmall.Error() {
+		t.Fatal("Error should be:", errPayloadTooSmall)
+	}
 }
