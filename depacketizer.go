34c6ce978ad576469ea43860fecc48fad612c7c8
Sean DuBois <sean@siobud.com>
2019-05-07 00:19:50 -0700

    Update Depacketizer interface to match codecs
    Codecs were updated but interface was not breaking
    the SampleBuilder

--- depacketizer.go
+++ depacketizer.go
@@ -2,5 +2,5 @@ package rtp
 
 // Depacketizer depacketizes a RTP payload, removing any RTP specific data from the payload
 type Depacketizer interface {
-	Unmarshal(packet *Packet) ([]byte, error)
+	Unmarshal(packet []byte) ([]byte, error)
 }
