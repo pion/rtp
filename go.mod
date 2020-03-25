fa01edc2dea88599a8aa7c4c5a938a12f59d9706
Atsushi Watanabe <atsushi.w@ieee.org>
2020-03-26 00:26:34 +0900

    Add AbsSendTimeExtension unmarshaller
    Implement Marshal, Unmarshal and absolute time.Time estimation.

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/pion/rtp
 
 go 1.12
-
-require github.com/stretchr/testify v1.5.1
--- packetizer_test.go
+++ packetizer_test.go
@@ -3,29 +3,9 @@ package rtp
 import (
 	"fmt"
 	"testing"
-	"time"
 
 	"github.com/pion/rtp/codecs"
-	"github.com/stretchr/testify/assert"
 )
-
-func TestNtpConversion(t *testing.T) {
-	loc := time.FixedZone("UTC-5", -5*60*60)
-
-	tests := []struct {
-		t time.Time
-		n uint64
-	}{
-		{t: time.Date(1985, time.June, 23, 4, 0, 0, 0, loc), n: 0xa0c65b1000000000},
-		{t: time.Date(1999, time.December, 31, 23, 59, 59, 500000, loc), n: 0xbc18084f0020c49b},
-		{t: time.Date(2019, time.March, 27, 13, 39, 30, 8675309, loc), n: 0xe04641e202388b88},
-	}
-
-	for _, in := range tests {
-		out := toNtpTime(in.t)
-		assert.Equal(t, in.n, out)
-	}
-}
 
 func TestPacketizer(t *testing.T) {
 	multiplepayload := make([]byte, 128)
