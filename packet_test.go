pion/rtp

commit 4b1b58251dc834458b8650376b674fe1613e84d7

Fix rtp header extensions marshaling/unmarshaling
* The length in the RTP Header extension is the number of 32 bit words
  in the extensions. I.E. with rfc 8285 multiple extensions is the size
  of the all the extensions divided by 4.
* The extensions must be be padded to 32-bit boundaries.

* Fix the tests to have a correct raw packet.
* Add some more tests.

packet.go
@@ -129,15 +129,14 @@ func (h *Header) Unmarshal(rawPacket []byte) error {
 
 		h.ExtensionProfile = binary.BigEndian.Uint16(rawPacket[currOffset:])
 		currOffset += 2
+		extensionLength := int(binary.BigEndian.Uint16(rawPacket[currOffset:])) * 4
+		currOffset += 2
 
 		switch h.ExtensionProfile {
 		// RFC 8285 RTP One Byte Header Extension
 		case extensionProfileOneByte:
-			extensionLength := int(binary.BigEndian.Uint16(rawPacket[currOffset:]))
-			currOffset += 2
-
-			i := 0
-			for i < extensionLength {
+			end := currOffset + extensionLength
+			for currOffset < end {
 				if rawPacket[currOffset] == 0x00 { // padding
 					currOffset++
 					continue
@@ -154,15 +153,12 @@ func (h *Header) Unmarshal(rawPacket []byte) error {
 				extension := Extension{id: extid, payload: rawPacket[currOffset : currOffset+len]}
 				h.Extensions = append(h.Extensions, extension)
 				currOffset += len
-				i++
 			}
+
 		// RFC 8285 RTP Two Byte Header Extension
 		case extensionProfileTwoByte:
-			extensionLength := int(binary.BigEndian.Uint16(rawPacket[currOffset:]))
-			currOffset += 2
-
-			i := 0
-			for i < extensionLength {
+			end := currOffset + extensionLength
+			for currOffset < end {
 				if rawPacket[currOffset] == 0x00 { // padding
 					currOffset++
 					continue
@@ -177,14 +173,9 @@ func (h *Header) Unmarshal(rawPacket []byte) error {
 				extension := Extension{id: extid, payload: rawPacket[currOffset : currOffset+len]}
 				h.Extensions = append(h.Extensions, extension)
 				currOffset += len
-
-				i++
 			}
 
 		default: // RFC3550 Extension
-			extensionLength := int(binary.BigEndian.Uint16(rawPacket[currOffset:])) * 4
-			currOffset += 2
-
 			if len(rawPacket) < currOffset+extensionLength {
 				return fmt.Errorf("RTP header size insufficient for extension length; %d < %d", len(rawPacket), currOffset+extensionLength)
 			}
@@ -271,21 +262,15 @@ func (h *Header) MarshalTo(buf []byte) (n int, err error) {
 		n += 4
 	}
 
-	// Calculate the size of the header by seeing how many bytes we're written.
-	// TODO This is a BUG but fixing it causes more issues.
-	h.PayloadOffset = n
-
 	if h.Extension {
+		extHeaderPos := n
 		binary.BigEndian.PutUint16(buf[n+0:n+2], h.ExtensionProfile)
-		n += 2
+		n += 4
+		startExtensionsPos := n
 
 		switch h.ExtensionProfile {
 		// RFC 8285 RTP One Byte Header Extension
 		case extensionProfileOneByte:
-			extSize := uint16(len(h.Extensions))
-			binary.BigEndian.PutUint16(buf[n:n+2], extSize)
-			n += 2
-
 			for _, extension := range h.Extensions {
 				buf[n] = extension.id<<4 | (uint8(len(extension.payload)) - 1)
 				n++
@@ -293,10 +278,6 @@ func (h *Header) MarshalTo(buf []byte) (n int, err error) {
 			}
 		// RFC 8285 RTP Two Byte Header Extension
 		case extensionProfileTwoByte:
-			extSize := uint16(len(h.Extensions))
-			binary.BigEndian.PutUint16(buf[n:n+2], extSize)
-			n += 2
-
 			for _, extension := range h.Extensions {
 				buf[n] = extension.id
 				n++
@@ -310,12 +291,20 @@ func (h *Header) MarshalTo(buf []byte) (n int, err error) {
 				//the payload must be in 32-bit words.
 				return 0, io.ErrShortBuffer
 			}
-			extSize := uint16(extlen / 4)
-			binary.BigEndian.PutUint16(buf[n:n+2], extSize)
-			n += 2
 			n += copy(buf[n:], h.Extensions[0].payload)
 		}
+
+		// calculate extensions size and round to 4 bytes boundaries
+		extSize := n - startExtensionsPos
+		roundedExtSize := ((extSize + 3) / 4) * 4
+
+		binary.BigEndian.PutUint16(buf[extHeaderPos+2:extHeaderPos+4], uint16(roundedExtSize/4))
+
+		// add padding to reach 4 bytes boundaries
+		n += roundedExtSize - extSize
 	}
+
+	h.PayloadOffset = n
 
 	return n, nil
 }
@@ -326,23 +315,27 @@ func (h *Header) MarshalSize() int {
 	size := 12 + (len(h.CSRC) * csrcLength)
 
 	if h.Extension {
-		size += 4
+		extSize := 4
 
 		switch h.ExtensionProfile {
 		// RFC 8285 RTP One Byte Header Extension
 		case extensionProfileOneByte:
 			for _, extension := range h.Extensions {
-				size += 1 + len(extension.payload)
+				extSize += 1 + len(extension.payload)
 			}
 		// RFC 8285 RTP Two Byte Header Extension
 		case extensionProfileTwoByte:
 			for _, extension := range h.Extensions {
-				size += 2 + len(extension.payload)
+				extSize += 2 + len(extension.payload)
 			}
 		default:
-			size += len(h.Extensions[0].payload)
+			extSize += len(h.Extensions[0].payload)
 		}
+
+		// extensions size must have 4 bytes boundaries
+		size += ((extSize + 3) / 4) * 4
 	}
+
 	return size
 }
 

packet_test.go
@@ -2,6 +2,7 @@ package rtp
 
 import (
 	"bytes"
+	"encoding/hex"
 	"reflect"
 	"testing"
 )
@@ -62,9 +63,8 @@ func TestBasic(t *testing.T) {
 		t.Errorf("TestBasic marshal: got %#v, want %#v", raw, rawPkt)
 	}
 
-	// TODO This is a BUG but without it, stuff breaks.
-	if p.PayloadOffset != 12 {
-		t.Errorf("wrong payload offset: %d != %d", p.PayloadOffset, 12)
+	if p.PayloadOffset != 20 {
+		t.Errorf("wrong payload offset: %d != %d", p.PayloadOffset, 20)
 	}
 }
 
@@ -108,7 +108,8 @@ func TestRFC8285OneByteExtension(t *testing.T) {
 
 	rawPkt := []byte{
 		0x90, 0xe0, 0x69, 0x8f, 0xd9, 0xc2, 0x93, 0xda, 0x1c, 0x64,
-		0x27, 0x82, 0xBE, 0xDE, 0x00, 0x01, 0x50, 0xAA, 0x98, 0x36, 0xbe, 0x88, 0x9e,
+		0x27, 0x82, 0xBE, 0xDE, 0x00, 0x01, 0x50, 0xAA, 0x00, 0x00,
+		0x98, 0x36, 0xbe, 0x88, 0x9e,
 	}
 	if err := p.Unmarshal(rawPkt); err != nil {
 		t.Fatal("Unmarshal err for valid extension")
@@ -131,13 +132,76 @@ func TestRFC8285OneByteExtension(t *testing.T) {
 		SSRC:           476325762,
 		CSRC:           []uint32{},
 	},
-		Payload: rawPkt[18:],
+		Payload: rawPkt[20:],
 		Raw:     rawPkt,
 	}
 
 	dstData, _ := p.Marshal()
 	if !bytes.Equal(dstData, rawPkt) {
-		t.Error("Marshal failed")
+		t.Errorf("Marshal failed raw \nMarshaled:\n%s\nrawPkt:\n%s", hex.Dump(dstData), hex.Dump(rawPkt))
+	}
+}
+
+func TestRFC8285OneByteTwoExtensionOfTwoBytes(t *testing.T) {
+	p := &Packet{}
+
+	//  0                   1                   2                   3
+	//  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+	// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	// |       0xBE    |    0xDE       |           length=1            |
+	// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	// |  ID   | L=0   |     data      |  ID   |  L=0  |   data...
+	// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	rawPkt := []byte{
+		0x90, 0xe0, 0x69, 0x8f, 0xd9, 0xc2, 0x93, 0xda, 0x1c, 0x64,
+		0x27, 0x82, 0xBE, 0xDE, 0x00, 0x01, 0x10, 0xAA, 0x20, 0xBB,
+		// Payload
+		0x98, 0x36, 0xbe, 0x88, 0x9e,
+	}
+	if err := p.Unmarshal(rawPkt); err != nil {
+		t.Fatal("Unmarshal err for valid extension")
+	}
+
+	ext1 := p.GetExtension(1)
+	ext1Expect := []byte{0xAA}
+	if !bytes.Equal(ext1, ext1Expect) {
+		t.Errorf("Extension has incorrect data. Got: %+v, Expected: %+v", ext1, ext1Expect)
+	}
+
+	ext2 := p.GetExtension(2)
+	ext2Expect := []byte{0xBB}
+	if !bytes.Equal(ext2, ext2Expect) {
+		t.Errorf("Extension has incorrect data. Got: %+v, Expected: %+v", ext2, ext2Expect)
+	}
+
+	// Test Marshal
+	p = &Packet{Header: Header{
+		Marker:           true,
+		Extension:        true,
+		ExtensionProfile: 0xBEDE,
+		Extensions: []Extension{
+			{1, []byte{
+				0xAA,
+			}},
+			{2, []byte{
+				0xBB,
+			}},
+		},
+		Version:        2,
+		PayloadOffset:  26,
+		PayloadType:    96,
+		SequenceNumber: 27023,
+		Timestamp:      3653407706,
+		SSRC:           476325762,
+		CSRC:           []uint32{},
+	},
+		Payload: rawPkt[20:],
+		Raw:     rawPkt,
+	}
+
+	dstData, _ := p.Marshal()
+	if !bytes.Equal(dstData, rawPkt) {
+		t.Errorf("Marshal failed raw \nMarshaled:\n%s\nrawPkt:\n%s", hex.Dump(dstData), hex.Dump(rawPkt))
 	}
 }
 
@@ -158,8 +222,9 @@ func TestRFC8285OneByteMultipleExtensionsWithPadding(t *testing.T) {
 	rawPkt := []byte{
 		0x90, 0xe0, 0x69, 0x8f, 0xd9, 0xc2, 0x93, 0xda, 0x1c, 0x64,
 		0x27, 0x82, 0xBE, 0xDE, 0x00, 0x03, 0x10, 0xAA, 0x21, 0xBB,
-		0xBB, 0x00, 0x00, 0x33, 0xCC, 0xCC, 0xCC, 0xCC, 0x98,
-		0x36, 0xbe, 0x88, 0x9e,
+		0xBB, 0x00, 0x00, 0x33, 0xCC, 0xCC, 0xCC, 0xCC,
+		// Payload
+		0x98, 0x36, 0xbe, 0x88, 0x9e,
 	}
 	if err := p.Unmarshal(rawPkt); err != nil {
 		t.Fatal("Unmarshal err for valid extension")
@@ -199,7 +264,7 @@ func TestRFC8285OneByteMultipleExtensions(t *testing.T) {
 	rawPkt := []byte{
 		0x90, 0xe0, 0x69, 0x8f, 0xd9, 0xc2, 0x93, 0xda, 0x1c, 0x64,
 		0x27, 0x82, 0xBE, 0xDE, 0x00, 0x03, 0x10, 0xAA, 0x21, 0xBB,
-		0xBB, 0x33, 0xCC, 0xCC, 0xCC, 0xCC,
+		0xBB, 0x33, 0xCC, 0xCC, 0xCC, 0xCC, 0x00, 0x00,
 		// Payload
 		0x98, 0x36, 0xbe, 0x88, 0x9e,
 	}
@@ -227,13 +292,13 @@ func TestRFC8285OneByteMultipleExtensions(t *testing.T) {
 		SSRC:           476325762,
 		CSRC:           []uint32{},
 	},
-		Payload: rawPkt[26:],
+		Payload: rawPkt[28:],
 		Raw:     rawPkt,
 	}
 
 	dstData, _ := p.Marshal()
 	if !bytes.Equal(dstData, rawPkt) {
-		t.Errorf("Marshal failed raw \nMarshaled: %+v,\nrawPkt:     %+v", dstData, rawPkt)
+		t.Errorf("Marshal failed raw \nMarshaled:\n%s\nrawPkt:\n%s", hex.Dump(dstData), hex.Dump(rawPkt))
 	}
 }
 
@@ -242,10 +307,10 @@ func TestRFC8285TwoByteExtension(t *testing.T) {
 
 	rawPkt := []byte{
 		0x90, 0xe0, 0x69, 0x8f, 0xd9, 0xc2, 0x93, 0xda, 0x1c, 0x64,
-		0x27, 0x82, 0x10, 0x00, 0x00, 0x01, 0x05, 0x18, 0xAA, 0xAA,
+		0x27, 0x82, 0x10, 0x00, 0x00, 0x07, 0x05, 0x18, 0xAA, 0xAA,
 		0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA,
 		0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA,
-		0xAA, 0xAA, 0x98, 0x36, 0xbe, 0x88, 0x9e,
+		0xAA, 0xAA, 0x00, 0x00, 0x98, 0x36, 0xbe, 0x88, 0x9e,
 	}
 	if err := p.Unmarshal(rawPkt); err != nil {
 		t.Fatal("Unmarshal err for valid extension")
@@ -270,13 +335,13 @@ func TestRFC8285TwoByteExtension(t *testing.T) {
 		SSRC:           476325762,
 		CSRC:           []uint32{},
 	},
-		Payload: rawPkt[42:],
+		Payload: rawPkt[44:],
 		Raw:     rawPkt,
 	}
 
 	dstData, _ := p.Marshal()
 	if !bytes.Equal(dstData, rawPkt) {
-		t.Errorf("Marshal failed \nExpected %v+ \nActual:  %v+", rawPkt, dstData)
+		t.Errorf("Marshal failed raw \nMarshaled:\n%s\nrawPkt:\n%s", hex.Dump(dstData), hex.Dump(rawPkt))
 	}
 }
 
@@ -343,7 +408,7 @@ func TestRFC8285TwoByteMultipleExtensionsWithLargeExtension(t *testing.T) {
 	// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 	rawPkt := []byte{
 		0x90, 0xe0, 0x69, 0x8f, 0xd9, 0xc2, 0x93, 0xda, 0x1c, 0x64,
-		0x27, 0x82, 0x10, 0x00, 0x00, 0x03, 0x01, 0x00, 0x02, 0x01,
+		0x27, 0x82, 0x10, 0x00, 0x00, 0x06, 0x01, 0x00, 0x02, 0x01,
 		0xBB, 0x03, 0x11, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC,
 		0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC,
 		// Payload

commit f18c884d79f51989a7bf77de16350c39084c1ad6

Fix extension padding on MarshalTo to dirty buffer
MarshalTo to non-zero buffer didn't clear padding bytes.
(regression from 4b1b5825)

packet.go
@@ -301,7 +301,10 @@ func (h *Header) MarshalTo(buf []byte) (n int, err error) {
 		binary.BigEndian.PutUint16(buf[extHeaderPos+2:extHeaderPos+4], uint16(roundedExtSize/4))
 
 		// add padding to reach 4 bytes boundaries
-		n += roundedExtSize - extSize
+		for i := 0; i < roundedExtSize-extSize; i++ {
+			buf[n] = 0
+			n++
+		}
 	}
 
 	h.PayloadOffset = n

packet_test.go
@@ -247,6 +247,33 @@ func TestRFC8285OneByteMultipleExtensionsWithPadding(t *testing.T) {
 	if !bytes.Equal(ext3, ext3Expect) {
 		t.Errorf("Extension has incorrect data. Got: %v+, Expected: %v+", ext3, ext3Expect)
 	}
+
+	rawPktReMarshal := []byte{
+		0x90, 0xe0, 0x69, 0x8f, 0xd9, 0xc2, 0x93, 0xda, 0x1c, 0x64,
+		0x27, 0x82, 0xBE, 0xDE, 0x00, 0x03, 0x10, 0xAA, 0x21, 0xBB,
+		0xBB, 0x33, 0xCC, 0xCC, 0xCC, 0xCC, 0x00, 0x00, // padding is moved to the end by re-marshaling
+		// Payload
+		0x98, 0x36, 0xbe, 0x88, 0x9e,
+	}
+	dstBuf := map[string][]byte{
+		"CleanBuffer": make([]byte, 1000),
+		"DirtyBuffer": make([]byte, 1000),
+	}
+	for i := range dstBuf["DirtyBuffer"] {
+		dstBuf["DirtyBuffer"][i] = 0xFF
+	}
+	for name, buf := range dstBuf {
+		buf := buf
+		t.Run(name, func(t *testing.T) {
+			n, err := p.MarshalTo(buf)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !bytes.Equal(buf[:n], rawPktReMarshal) {
+				t.Errorf("Marshal failed raw \nMarshaled:\n%s\nrawPkt:\n%s", hex.Dump(buf[:n]), hex.Dump(rawPktReMarshal))
+			}
+		})
+	}
 }
 
 func TestRFC8285OneByteMultipleExtensions(t *testing.T) {