3ea35019945910320f917f2aef8a9fbc4ace4d83
Tarrence van As <tarrence13@gmail.com>
2020-05-19 08:51:29 -0500

    Fix AudioLevel Extension
    * ID/len handled by rtp package now

--- audiolevelextension_test.go
+++ audiolevelextension_test.go
@@ -10,52 +10,16 @@ func TestAudioLevelExtensionTooSmall(t *testing.T) {
 
 	rawData := []byte{}
 
-	if err := a.Unmarshal(rawData); err != errInvalidSize {
-		t.Fatal("err != errInvalidSize")
+	if err := a.Unmarshal(rawData); err != errTooSmall {
+		t.Fatal("err != errTooSmall")
 	}
 }
 
-func TestAudioLevelExtensionTooBig(t *testing.T) {
-	a := AudioLevelExtension{}
-
-	rawData := []byte{
-		0x00, 0x00, 0x00, 0x00, 0x00,
-	}
-
-	if err := a.Unmarshal(rawData); err != errInvalidSize {
-		t.Fatal("err != errInvalidSize")
-	}
-}
-
-func TestAudioLevelOneByteExtensionInvalidLength(t *testing.T) {
-	a := AudioLevelExtension{}
-
-	rawData := []byte{
-		0x31, 0x88,
-	}
-
-	if err := a.Unmarshal(rawData); err != errInvalidExtensonLength {
-		t.Fatal("err != errInvalidExtensonLength")
-	}
-}
-
-func TestAudioLevelTwoByteExtensionInvalidLength(t *testing.T) {
-	a := AudioLevelExtension{}
-
-	rawData := []byte{
-		0x30, 0x00, 0x00, 0x00,
-	}
-
-	if err := a.Unmarshal(rawData); err != errInvalidExtensonLength {
-		t.Fatal("err != errInvalidExtensonLength")
-	}
-}
-
-func TestAudioLevelOneByteExtensionVoiceTrue(t *testing.T) {
+func TestAudioLevelExtensionVoiceTrue(t *testing.T) {
 	a1 := AudioLevelExtension{}
 
 	rawData := []byte{
-		0x30, 0x88,
+		0x88,
 	}
 
 	if err := a1.Unmarshal(rawData); err != nil {
@@ -63,7 +27,6 @@ func TestAudioLevelOneByteExtensionVoiceTrue(t *testing.T) {
 	}
 
 	a2 := AudioLevelExtension{
-		ID:    3,
 		Level: 8,
 		Voice: true,
 	}
@@ -78,11 +41,11 @@ func TestAudioLevelOneByteExtensionVoiceTrue(t *testing.T) {
 	}
 }
 
-func TestAudioLevelOneByteExtensionVoiceFalse(t *testing.T) {
+func TestAudioLevelExtensionVoiceFalse(t *testing.T) {
 	a1 := AudioLevelExtension{}
 
 	rawData := []byte{
-		0x30, 0x8,
+		0x8,
 	}
 
 	if err := a1.Unmarshal(rawData); err != nil {
@@ -90,7 +53,6 @@ func TestAudioLevelOneByteExtensionVoiceFalse(t *testing.T) {
 	}
 
 	a2 := AudioLevelExtension{
-		ID:    3,
 		Level: 8,
 		Voice: false,
 	}
@@ -105,45 +67,13 @@ func TestAudioLevelOneByteExtensionVoiceFalse(t *testing.T) {
 	}
 }
 
-func TestAudioLevelOneByteExtensionLevelOverflow(t *testing.T) {
+func TestAudioLevelExtensionLevelOverflow(t *testing.T) {
 	a := AudioLevelExtension{
-		ID:    3,
 		Level: 128,
 		Voice: false,
 	}
 
 	if _, err := a.Marshal(); err != errAudioLevelOverflow {
 		t.Fatal("err != errAudioLevelOverflow")
-	}
-}
-
-func TestAudioLevelTwoByteExtensionVoiceFalse(t *testing.T) {
-	a1 := AudioLevelExtension{}
-
-	oneByteRawData := []byte{
-		0x30, 0x8,
-	}
-
-	twoByteRawData := []byte{
-		0x3, 0x1, 0x8, 0x0,
-	}
-
-	if err := a1.Unmarshal(twoByteRawData); err != nil {
-		t.Fatal("Unmarshal error on extension data")
-	}
-
-	a2 := AudioLevelExtension{
-		ID:    3,
-		Level: 8,
-		Voice: false,
-	}
-
-	if a1 != a2 {
-		t.Error("Unmarshal failed")
-	}
-
-	dstData, _ := a2.Marshal()
-	if !bytes.Equal(dstData, oneByteRawData) {
-		t.Error("Marshal failed")
 	}
 }
