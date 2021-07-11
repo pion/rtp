0ec84cdb3032fbbfa5fc94ae535ae280ca1161c3
Juliusz Chroboczek <jch@irif.fr>
2021-07-11 15:48:33 +0200

    Add comment explaining why PartitionHeadChecker is an interface

--- partitionheadchecker.go
+++ partitionheadchecker.go
@@ -1,6 +1,8 @@
 package rtp
 
 // PartitionHeadChecker is the interface that checks whether the packet is keyframe or not
+// This is essentially func([]byte) bool, but for compatibility reasons is
+// kept as an interface.  The analogous PartitionTailChecker is just a function.
 type PartitionHeadChecker interface {
 	IsPartitionHead([]byte) bool
 }
