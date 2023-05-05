1a23b62560763ac23b3f31966688bc0b38db09d8
Steffen Vogel <post@steffenvogel.de>
2023-05-05 14:41:20 +0200

    Make repo REUSE compliant

--- pkg/frame/av1.go
+++ pkg/frame/av1.go
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+// SPDX-License-Identifier: MIT
+
 // Package frame provides code to construct complete media frames from packetized media
 package frame
 
