42ad1265abea614327dd29ed02251929e8363ce7
Pion <59523206+pionbot@users.noreply.github.com>
2026-01-22 08:44:34 +0000

    Update CI configs to v0.11.37
    Update lint scripts and CI configs.

--- codecs/h264/spreader.go
+++ codecs/h264/spreader.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+// SPDX-FileCopyrightText: 2026 The Pion community <https://pion.ly>
 // SPDX-License-Identifier: MIT
 
 // Package h264 provides helpers for working with H264 Bitstreams
